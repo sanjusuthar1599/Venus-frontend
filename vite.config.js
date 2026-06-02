2ceb7c0bad6a1365cfcb3c576ca8707c1b4179eb
Sanjay <Sanjusuthar1599@gmail.com>
2026-06-02 19:06:19 +0530

    fix render allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     tailwindcss(),
+//     react()
+//   ],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()
   ],
+
+  preview: {
+    host: '0.0.0.0',
+    allowedHosts: true,
+  }
 })
